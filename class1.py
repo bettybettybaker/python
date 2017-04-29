cd1a3c2b15699eb72fb178067cb958d2984273de
Betty Baker <betty@heysparkbox.com>
2017-04-29 11:19:56 -0400

    chained conditionals

--- class1.py
+++ class1.py
@@ -1,15 +1,6 @@
-to_do_list = ['Make the bed', 'Cook dinner', 'Feed the dog', 'Buy groceries']
-to_do_list.append('buy potatoes')
-to_do_list.append('clean car')
-print to_do_list
-
-# prints the first chore on the list
-print to_do_list[0]
-
-# prints the third chore on the list
-print to_do_list[2]
-
-print len(to_do_list)
-
-# prints the last chore on the list
-print to_do_list[len(to_do_list) - 1]
+if x < 0:
+    print "x is negative"
+elif x > 0:
+    print "x is positive"
+else:
+    print "x is 0"
