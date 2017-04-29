c888d4e5e6f8363c57f302b70c5919f39205b18a
Betty Baker <betty@heysparkbox.com>
2017-04-29 10:03:29 -0400

    Age Demo

--- class1.py
+++ class1.py
@@ -1,1 +1,7 @@
-print 'Hello World!'
+name = raw_input("What is your name? ")
+age = raw_input("How old are you? ")
+age_int = int(age)
+print "Your name is " + name + " and you are " + age + " years old."
+
+type(age)
+type(age_int)
